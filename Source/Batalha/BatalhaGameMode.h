30b709f59998b9573dbb11b83a012a785c46a699
Matheus Ferreira <matheusfsantos.152@gmail.com>
2023-09-19 12:37:48 -0300

    add

---
+++ Source/Batalha/BatalhaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BatalhaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABatalhaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABatalhaGameMode();
+};
+
+
+
