2cd4509973326f536c39d8ed8275d305da6c0842
Matheus Ferreira <matheusfsantos.152@gmail.com>
2023-11-27 09:40:20 -0300

    Amem

---
+++ Source/Femboy/FemboyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FemboyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEMBOY_API AFemboyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
